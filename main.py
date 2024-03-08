31f69f0464c2136d25561f38a63b7483e2d0c225
Lev <tab.lev.008@gmail.com>
2024-03-08 19:34:01 +0300

    Поиск по названию

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import requests
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QLineEdit
 from PyQt5.QtGui import QFont, QPixmap
 import time
 import keyboard
@@ -17,6 +17,17 @@ def Check_key():
 def Request(x, y, size, type):
     map_request = f"http://static-maps.yandex.ru/1.x/?ll={x},{y}&spn={size},0.002&l={type}"
     return requests.get(map_request)
+
+def Poisk_po_organiachiiam(size, name):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": name,
+        "format": "json"}
+
+    return requests.get(geocoder_api_server, params=geocoder_params)
+
 
 
 class Main_menu(QWidget):
@@ -46,7 +57,15 @@ class Main_menu(QWidget):
         self.picture = QPixmap('map.png')
         self.labl = QLabel(self)
         self.labl.setPixmap(self.picture)
-        self.labl.setGeometry(0, 0, 800, 500)
+        self.labl.setGeometry(0, 0, 600, 450)
+
+        self.line_edit = QLineEdit(self)
+        self.line_edit.setGeometry(250, 475, 150, 50)
+
+        search_btn = QPushButton('Искать', self)
+        search_btn.setGeometry(250, 525, 150, 50)
+        search_btn.setFont(QFont('Times', 14))
+        search_btn.clicked.connect(self.search)
 
         move_up_btn = QPushButton('UP', self)
         move_up_btn.setGeometry(75, 475, 50, 50)
@@ -77,7 +96,7 @@ class Main_menu(QWidget):
 
     def up(self):
         self.y += self.size * 1.5
-        response = Request(self.x, self.y, 0.002, 'map')
+        response = Request(self.x, self.y, self.size, 'map')
         with open(self.map_file, "wb") as file:
             file.write(response.content)
         self.picture = QPixmap('map.png')
@@ -85,7 +104,7 @@ class Main_menu(QWidget):
 
     def down(self):
         self.y -= self.size * 1.5
-        response = Request(self.x, self.y, 0.002, 'map')
+        response = Request(self.x, self.y, self.size, 'map')
         with open(self.map_file, "wb") as file:
             file.write(response.content)
         self.picture = QPixmap('map.png')
@@ -93,7 +112,7 @@ class Main_menu(QWidget):
 
     def left(self):
         self.x -= self.size * 3
-        response = Request(self.x, self.y, 0.002, 'map')
+        response = Request(self.x, self.y, self.size, 'map')
         with open(self.map_file, "wb") as file:
             file.write(response.content)
         self.picture = QPixmap('map.png')
@@ -101,17 +120,23 @@ class Main_menu(QWidget):
 
     def right(self):
         self.x += self.size * 3
-        response = Request(self.x, self.y, 0.002, 'map')
+        response = Request(self.x, self.y, self.size, 'map')
+        with open(self.map_file, "wb") as file:
+            file.write(response.content)
+        self.picture = QPixmap('map.png')
+        self.labl.setPixmap(self.picture)
+
+    def search(self):
+        toponyms = Poisk_po_organiachiiam(self.size, self.line_edit.text()).json()
+        self.x, self.y = toponyms["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"].split()
+        response = Request(self.x, self.y, self.size, 'map')
         with open(self.map_file, "wb") as file:
             file.write(response.content)
         self.picture = QPixmap('map.png')
         self.labl.setPixmap(self.picture)
 
 
-
-
 if __name__ == '__main__':
-    sys.excepthook = except_hook
     app = QApplication(sys.argv)
     window_menu = Main_menu()
 
